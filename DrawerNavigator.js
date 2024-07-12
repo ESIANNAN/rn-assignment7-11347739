bf163a2aae5a6e8ed7df0fb4c70385d2c6300118
Philomina Annan- 11347739 <pannan027@st.ug.edu.gh>
2024-07-12 16:39:05 +0000

    Create DrawerNavigator.js

---
+++ DrawerNavigator.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import Homescreen from './HomeScreen';
+
+const Drawer = createDrawerNavigator();
+
+const DrawerNavigator = () => {
+  return (
+    <Drawer.Navigator initialRouteName="Home">
+      <Drawer.Screen name="Home" component={Homescreen} />
+    
+    </Drawer.Navigator>
+  );
+};
+
+export default DrawerNavigator;
