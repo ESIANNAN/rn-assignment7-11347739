52cdb33766d7ff92e63d193ae143db9384ae5981
Philomina Annan- 11347739 <pannan027@st.ug.edu.gh>
2024-07-12 17:01:42 +0000

    Update App.js
    Added the side bar using the drawer navigation component

--- App.js
+++ App.js
@@ -1,26 +1,35 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 import HomeScreen from './HomeScreen';
 import Cart from './Cart';
 import ProductDetailScreen from './ProductDetailScreen';
 
 const Stack = createStackNavigator();
+const Drawer = createDrawerNavigator();
 
 export default function App() {
   const [cart, setCart] = React.useState([]);
 
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" options={{ title: 'Home' }}>
-          {props => <HomeScreen {...props} cart={cart} setCart={setCart} />}
-        </Stack.Screen>
-        <Stack.Screen name="Cart" options={{ title: 'Cart' }}>
-          {props => <Cart {...props} cart={cart} setCart={setCart} />}
-        </Stack.Screen>
-        <Stack.Screen name="ProductDetail" component={ProductDetailScreen} options={{ title: 'Product Detail' }} />
-      </Stack.Navigator>
+      <Drawer.Navigator initialRouteName="Home">
+        <Drawer.Screen name="Home" options={{ title: 'Home' }}>
+          {props => (
+            <Stack.Navigator initialRouteName="Home">
+              <Stack.Screen name="Home" options={{ title: 'Home' }}>
+                {props => <HomeScreen {...props} cart={cart} setCart={setCart} />}
+              </Stack.Screen>
+              <Stack.Screen name="ProductDetail" component={ProductDetailScreen} options={{ title: 'Product Detail' }} />
+            </Stack.Navigator>
+          )}
+        </Drawer.Screen>
+        <Drawer.Screen name="Cart" options={{ title: 'Cart' }}>
+          {props => <Cart {...props} cart={cart} />}
+        </Drawer.Screen>
+
+      </Drawer.Navigator>
     </NavigationContainer>
   );
 }
